d4206d325a3e92b5b76648f1d7e266c2fb68d0f4
irembuz <irembuz7@gmail.com>
2022-07-04 14:32:29 +0300

    update Login screen input fields

--- react-native-podcast-app/screens/Login.js
+++ react-native-podcast-app/screens/Login.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Text, View, StyleSheet, TextInput, Pressable } from "react-native";
+import { Text, View, StyleSheet, Pressable } from "react-native";
+import { TextInput } from "react-native-paper";
 
 import Logo from "../components/Logo";
 
@@ -9,13 +10,32 @@ const Login = () => {
       <View style={styles.contentContainer}>
         <Logo />
         <Text style={styles.text}>Episodic series of digital audio</Text>
-
-        <TextInput style={styles.input} placeholder="E-mail address" />
+        <TextInput
+          style={styles.input}
+          placeholder="E-mail address"
+          mode='outlined'
+          left={<TextInput.Icon name="email" color="#898F97" />}
+          theme={{
+            colors: {
+              text: "#898F97",
+              placeholder: "#898F97",
+            },
+          }}
+        />
         <TextInput
           style={styles.input}
           placeholder="Password"
+          mode='outlined'
           secureTextEntry
+          left={<TextInput.Icon name="key" color="#898F97" />}
+          theme={{
+            colors: {
+              text: "#898F97",
+              placeholder: "#898F97",
+            },
+          }}
         />
+        {/* Login Button */}
         <Pressable style={styles.button}>
           <Text style={styles.buttonText}>Login</Text>
         </Pressable>
@@ -38,12 +58,12 @@ const styles = StyleSheet.create({
     padding: 32,
   },
   input: {
-    backgroundColor: "#09121C",
     fontSize: 15,
     marginTop: 16,
     paddingHorizontal: 16,
     paddingVertical: 14,
-    borderRadius: 25,
+    height: 30,
+    backgroundColor: "value",
   },
   button: {
     alignItems: "center",
@@ -53,6 +73,7 @@ const styles = StyleSheet.create({
     borderRadius: 25,
     elevation: 3,
     backgroundColor: "#3369FF",
+    marginTop: 24,
   },
   buttonText: {
     fontSize: 16,
